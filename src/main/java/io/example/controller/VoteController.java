fc83441731cabe6c2b84c0ddd6044bc2a2bfb786
kernel-panic-42 <kernel_pan1c@fastmail.net>
2021-12-02 10:14:34 +1300

    initial respository handlers and controllers

--- src/main/java/io/example/controller/VoteController.java
+++ src/main/java/io/example/controller/VoteController.java
@@ -5,6 +5,7 @@ import javax.inject.Inject;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,11 +24,16 @@ public class VoteController {
 	public ResponseEntity<?> createVote(@PathVariable Long pollId, @RequestBody Vote vote) {
 		vote = voteRepository.save(vote);
 		HttpHeaders responseHeaders = new HttpHeaders();
-		responseHeaders.setLocation(ServletUriComponentsBuilder	.fromCurrentRequest()
-																.path("/{id}")
-																.buildAndExpand(vote.getId())
-																.toUri());
+		responseHeaders.setLocation(ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(vote.getId())
+				.toUri());
 		return new ResponseEntity<>(null, responseHeaders, HttpStatus.CREATED);
+	}
+
+	@GetMapping("/polls/{pollId}/votes")
+	public Iterable<Vote> getAllVotes(@PathVariable Long pollId) {
+		return voteRepository.findByPoll(pollId);
 	}
 
 }
--- src/main/java/io/example/repository/VoteRepository.java
+++ src/main/java/io/example/repository/VoteRepository.java
@@ -1,9 +1,13 @@
 package io.example.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import io.example.domain.Vote;
 
 public interface VoteRepository extends CrudRepository<Vote, Long> {
+
+    @Query(value = "select v.* from Option o, Vote v where o.POLL_ID = ?1 and v.OPTION_ID = o.OPTION_ID", nativeQuery = true)
+    public Iterable<Vote> findByPoll(Long PollId);
 
 }
